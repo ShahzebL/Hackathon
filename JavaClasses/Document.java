4788b675c1a56d4dca5f7fde2669090994e7b7f0
A.L <22alexl@students.harker.org>
2019-03-30 14:53:11 -0700

    Document.java
    a

--- JavaClasses/Document.java
+++ JavaClasses/Document.java
@@ -5,7 +5,7 @@ public class Document
      private BufferedReader f;
      private PrintWriter outf;
      private ArrayList<String> wordDictionary;
-
+    private ArrayList<String> humanText;
      public void readWords(String filename)
      {
          f = new BufferedReader(new FileReader(filename));
@@ -32,6 +32,18 @@ public class Document
          ArrayList<String> errors = new ArrayList<String>();
 
      }
+    public void retrieveText(String filename)
+     {
+         f = new BufferedReader(new FileReader(filename));
+         StringTokenizer st = new StringTokenizer(f.readLine());
+         int numWords = Integer.parseInt(st.nextToken());
 
+         humanText = new String[numWords];
 
+         for(int i = 0; i < numWords; i++)
+         {
+             st = new StringTokenizer(f.readLine());
+             humanText.add(st.nextToken());
+         }
+     }
 }
