27a84a0f37b84259e33bb8e61837c81f904876a8
Dawson Chen <22dawsonc@students.harker.org>
2019-03-30 19:21:32 -0700

    connect .js to Java classes

--- Web/index.js
+++ Web/index.js
@@ -1,17 +1,35 @@
 var express = require('express');
-index = express();
+const index = express();
+const bodyParser = require("body-parser");
+const fs = require("fs");
 index.set("view engine", "ejs");
+const { promisify } = require('util')
 
-index.get("/", function(req, res) {
+index.get("/", async function (req, res) {
     console.log("run");
-    res.render("index");
-    
+    await res.render("index");
+
 });
 
-index.get("/revise", function(req, res){
-    console.log("gotta do sometin");
+index.post("/revise", bodyParser(), async function (req, res) {
+    const body = req.body.essay;
+
+    let err = await promisify(fs.writeFile)('../in.txt', body)
+    if (err) res.status(500).send("Error while writing to file.")
+
+    // fancy java stuff to process. maybe use exec?
+    // then respond back
+
+    // await res.render("comments")
+    res.status(200).send("Success. Now I should really do something good.")
+    processText();
 })
 
-index.listen(3000, function(){
+var processText = function() {
+    var myClass = Java.type("JavaClasses.ReviseTester");
+    myClass.main();
+}
+
+index.listen(3001, function () {
     console.log("server running");
 });
